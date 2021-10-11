8f207760da78df258d66a2fe239b261a5deb123a
Lisa <llvvss@icloud.com>
2021-10-11 13:43:45 -0600

    adding a sign up form and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "resgistrations#create"
+
   root to: "main#index"
 end
