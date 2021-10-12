ef39739637344aa59a6e92aa71132e9c990c03d7
Lisa <llvvss@icloud.com>
2021-10-12 15:26:20 -0600

    logging out users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 end
