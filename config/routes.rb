fa8119511897f142ccf63dfc1fa7c21fcc80f689
Lisa <llvvss@icloud.com>
2021-10-11 19:58:53 -0600

    handling sign up errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   get "sign_up", to: "registrations#new"
-  post "sign_up", to: "resgistrations#create"
+  post "sign_up", to: "registrations#create"
 
   root to: "main#index"
 end
