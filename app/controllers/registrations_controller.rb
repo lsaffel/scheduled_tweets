4351363083b69752f3c4e90c7677598e669127b6
Lisa <llvvss@icloud.com>
2021-10-12 14:50:53 -0600

    login with session cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,12 +6,13 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Successfully created account"
         else
             render :new
         end
     end
-      
+
     private
 
     def user_params
