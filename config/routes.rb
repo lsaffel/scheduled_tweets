070c2750b9b1901adbed5511cd4769e86d3b6fc5
Lisa <llvvss@icloud.com>
2021-10-16 21:15:30 -0600

    adding forgot your password

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,8 @@ Rails.application.routes.draw do
   
   delete "logout", to: "sessions#destroy"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   root to: "main#index"
 end
