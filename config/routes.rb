35fbd12342d2426db81491888faf507254a616f9
Lisa <llvvss@icloud.com>
2021-10-21 21:51:19 -0600

    reset password token mailer

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
 
   get "password/reset", to: "password_resets#new"
-  post "password/reset", to: "password_resets#create"
+  post "password/reset", to: "password_resets#create"  
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   root to: "main#index"
 end
